38f293a6e6370df9fb1f5481e92b63b2a9a4eccb
Ryu0n <hsung951027@gmail.com>
2023-04-16 11:05:40 +0900

    🧪 [TEST] Airflow test

--- dag-scraping-naver-blog/dag-scraping-naver-blog.py
+++ dag-scraping-naver-blog/dag-scraping-naver-blog.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from airflow.models import DAG
 
 from airflow.operators.python import PythonOperator
@@ -8,6 +10,7 @@ from airflow.providers.http.operators.http import SimpleHttpOperator
 with DAG(
     dag_id='dag-scraping-naver-blog',
     schedule_interval='@daily',
+    start_date=datetime(2023, 4, 1),
     catchup=False
 ) as dag:
     
