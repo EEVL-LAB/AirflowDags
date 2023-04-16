69d84c17cdbdb24f5a8a73493e146a646b1a09f4
Ryu0n <hsung951027@gmail.com>
2023-04-17 02:11:24 +0900

    ✨ [REFACTOR] 네이버 스크래핑 DAG 수정

--- dag-scraping-naver-blog/dag-scraping-naver-blog.py
+++ dag-scraping-naver-blog/dag-scraping-naver-blog.py
@@ -1,29 +1,31 @@
-import json
-
-from datetime import datetime
-
-from airflow.models import DAG
-
-from airflow.operators.python import PythonOperator
-# from airflow.providers.http.sensors.http import HttpSensor
-# from airflow.providers.http.operators.http import SimpleHttpOperator
-
 import requests
+import datetime
+from typing import List
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+
+
+target_keywords = [
+    "윤석열",
+    "오늘수거"
+]
 
 
 def request_naver_blog_into_kafka_provider(
-    target_keyword: str,
-    start_date: str,
-    end_date: str
+    target_keywords: List[str]
 ):
-    requests.post(
-        url='http://scraping.eevl.studio:8080/scraping_naver_blog',
-        json={
-            'target_keyword': target_keyword,
-            'start_date': start_date,
-            'end_date': end_date
-        }
-    )
+    today = datetime.date.today()
+    delta = datetime.timedelta(days=1)
+    date = str(today - delta)
+    for target_keyword in target_keywords:
+        requests.post(
+            url='http://scraping.eevl.studio:8080/scraping_naver_blog',
+            json={
+                'target_keyword': target_keyword,
+                'start_date': date,
+                'end_date': date
+            }
+        )
 
 
 with DAG(
@@ -33,36 +35,11 @@ with DAG(
     catchup=False
 ) as dag:
     
-    # is_scraping_naver_blog_api_available = HttpSensor(
-    #     task_id='is-scraping-naver-blog-api-available',
-    #     http_conn_id='scraping-into-kafka-provider',
-    #     endpoint='/scraping_naver_blog'
-    # )
-    
-    # scraping_naver_blog_into_kafka_provider = SimpleHttpOperator(
-    #     task_id='scraping-naver-blog-into-kafka-provider',
-    #     http_conn_id='scraping-into-kafka-provider',
-    #     endpoint='/scraping_naver_blog',
-    #     method='POST',
-    #     data={
-    #         'target_keyword': '윤석열',
-    #         'start_date': '2022-04-14',
-    #         'end_date': '2022-06-14'
-    #     },
-    #     response_filter=lambda res: json.loads(res.text),
-    #     log_response=True
-    # )
-    
-    # is_scraping_naver_blog_api_available >> scraping_naver_blog_into_kafka_provider
-    # scraping_naver_blog_into_kafka_provider
-    
     scraping_naver_blog_into_kafka_provider = PythonOperator(
         task_id='scraping_naver_blog_into_kafka_provider',
         python_callable=request_naver_blog_into_kafka_provider,
         op_kwargs={
-            "target_keyword": "윤석열",
-            "start_date": "2023-01-01",
-            "end_date": "2023-04-15"
+            "target_keyword": target_keywords
         }
     )
     
