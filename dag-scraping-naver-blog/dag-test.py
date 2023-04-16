81e00778ce6a993494d21c889c07c71d03782731
Ryu0n <hsung951027@gmail.com>
2023-04-16 13:12:20 +0900

    🧪 [TEST] Airflow test

--- dag-scraping-naver-blog/dag-test.py
+++ dag-scraping-naver-blog/dag-test.py
@@ -3,6 +3,11 @@ from datetime import datetime
 from airflow.models import DAG
 
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.operators.python import PythonOperator
+
+
+def print_hi():
+    print('hi')
 
 
 with DAG(
@@ -12,10 +17,17 @@ with DAG(
     catchup=False
 ) as dag:
     
-    is_available = HttpSensor(
-        task_id='is-scraping-naver-blog-api-available',
-        http_conn_id='test-connection',
-        endpoint='/'
+    # is_available = HttpSensor(
+    #     task_id='is-scraping-naver-blog-api-available',
+    #     http_conn_id='test-connection',
+    #     endpoint='/'
+    # )
+    
+    # is_available
+    
+    h = PythonOperator(
+        task_id='print-hi',
+        python_callable=print_hi
     )
     
-    is_available
+    h
