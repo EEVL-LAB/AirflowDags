2452ad1f89122dc8eef1b563eaf3731a505aeb07
Ryu0n <hsung951027@gmail.com>
2023-04-16 10:58:02 +0900

    🧪 [TEST] Airflow test

---
+++ dag-scraping-naver-blog/dag-scraping-naver-blog.py
@@ -0,0 +1,26 @@
+from airflow.models import DAG
+
+from airflow.operators.python import PythonOperator
+
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+
+with DAG(
+    dag_id='dag-scraping-naver-blog',
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+    
+    scraping_naver_blog_into_kafka_provider = SimpleHttpOperator(
+        task_id='scraping-naver-blog-into-kafka-provider',
+        http_conn_id='scraping-into-kafka-provider',
+        endpoint='scraping_naver_blog',
+        request_params={
+            'target_keyword': '윤석열',
+            'start_date': '2022-04-14',
+            'end_date': '2022-06-14'
+        },
+        log_response=True
+    )
+    
+    scraping_naver_blog_into_kafka_provider
