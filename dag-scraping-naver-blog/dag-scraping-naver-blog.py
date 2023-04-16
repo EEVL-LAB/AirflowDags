7ad01ddb1c9285d5de48b1ae2cd0f40e92a2a4ea
Ryu0n <hsung951027@gmail.com>
2023-04-17 00:06:28 +0900

    🧪 [TEST] Airflow test

--- dag-scraping-naver-blog/dag-scraping-naver-blog.py
+++ dag-scraping-naver-blog/dag-scraping-naver-blog.py
@@ -8,7 +8,22 @@ from airflow.operators.python import PythonOperator
 # from airflow.providers.http.sensors.http import HttpSensor
 # from airflow.providers.http.operators.http import SimpleHttpOperator
 
-from scripts.api import *
+import requests
+
+
+def request_naver_blog_into_kafka_provider(
+    target_keyword: str,
+    start_date: str,
+    end_date: str
+):
+    requests.post(
+        url='http://scraping.eevl.studio:8080/scraping_naver_blog',
+        data={
+            'target_keyword': target_keyword,
+            'start_date': start_date,
+            'end_date': end_date
+        }
+    )
 
 
 with DAG(
