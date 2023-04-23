6d9cd60751a86a45fd46811ec8fadff68b3012f9
Ryu0n <hsung951027@gmail.com>
2023-04-23 21:17:16 +0900

    test

---
+++ dag-scraping-naver-blog/dag-test.py
@@ -0,0 +1,38 @@
+import time
+import requests
+import datetime
+from typing import List
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+
+
+def request_naver_blog_into_kafka_provider(
+    target_keywords: List[str]
+):
+    while True:
+        time.sleep(60*10)
+        break
+
+
+with DAG(
+    dag_id='dag-scraping-naver-blog',
+    schedule_interval='@daily',
+    start_date=datetime.datetime(2023, 4, 1),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(days=1)
+) as dag:
+    
+    scraping_naver_blog_into_kafka_provider = PythonOperator(
+        task_id='scraping_naver_blog_into_kafka_provider',
+        execution_timeout=datetime.timedelta(days=1),
+        sla=datetime.timedelta(days=1),
+        python_callable=request_naver_blog_into_kafka_provider,
+        op_kwargs={
+            "target_keywords": [
+                '윤석열',
+                '오늘수거'
+            ]
+        }
+    )
+    
+    scraping_naver_blog_into_kafka_provider
