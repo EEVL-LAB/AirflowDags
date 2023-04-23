50dd431e5dffbbe0d680d0ed242bc6024299915d
Ryu0n <hsung951027@gmail.com>
2023-04-23 12:25:12 +0900

    ✨ [FEAT] 빅카인즈 DAG 추가

---
+++ dag-scraping-bigkinds/dag-scraping-bigkinds.py
@@ -0,0 +1,45 @@
+import requests
+import datetime
+from typing import List
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+
+
+def request_bigkinds_into_kafka_provider(
+    target_keywords: List[str]
+):
+    today = datetime.date.today()
+    delta = datetime.timedelta(days=1)
+    date = str(today - delta)
+    for target_keyword in target_keywords:
+        response = requests.post(
+            url='http://scraping.eevl.studio:8080/scraping_bigkinds',
+            json={
+                'target_keyword': target_keyword,
+                'start_date': date,
+                'end_date': date,
+                "limit": 100
+            }
+        )
+        print(response)
+
+
+with DAG(
+    dag_id='dag-scraping-bigkinds',
+    schedule_interval='@daily',
+    start_date=datetime.datetime(2023, 4, 1),
+    catchup=False
+) as dag:
+    
+    scraping_bigkinds_into_kafka_provider = PythonOperator(
+        task_id='scraping_bigkinds_into_kafka_provider',
+        python_callable=request_bigkinds_into_kafka_provider,
+        op_kwargs={
+            "target_keywords": [
+                '윤석열',
+                '오늘수거'
+            ]
+        }
+    )
+    
+    scraping_bigkinds_into_kafka_provider
